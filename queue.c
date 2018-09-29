abfcdabb56db4c9d1bf696c123da26f65784d19e
AlecJY <ae40515@yahoo.com.tw>
2018-09-29 14:10:17 +0800

    Check if malloc() returned NULL in q_new()
    If malloc() returned NULL, directly return q to prevent using NULL
    porinter.

--- queue.c
+++ queue.c
@@ -26,6 +26,9 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
+    if (q == NULL) {
+        return q;
+    }
     q->head = NULL;
     return q;
 }
